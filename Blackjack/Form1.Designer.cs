488039dab42ca993f214c3ebcc884c64012f6589
PC-16 <it.pgee.burgas@gmail.com>
2025-07-07 11:24:28 +0300

    Basic game (no ui)

--- Blackjack/Form1.Designer.cs
+++ Blackjack/Form1.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblPlayerScore = new System.Windows.Forms.Label();
+            this.lblDealerScore = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblPlayerScore
+            // 
+            this.lblPlayerScore.AutoSize = true;
+            this.lblPlayerScore.Location = new System.Drawing.Point(944, 649);
+            this.lblPlayerScore.Name = "lblPlayerScore";
+            this.lblPlayerScore.Size = new System.Drawing.Size(49, 16);
+            this.lblPlayerScore.TabIndex = 0;
+            this.lblPlayerScore.Text = "Player:";
+            // 
+            // lblDealerScore
+            // 
+            this.lblDealerScore.AutoSize = true;
+            this.lblDealerScore.Location = new System.Drawing.Point(234, 649);
+            this.lblDealerScore.Name = "lblDealerScore";
+            this.lblDealerScore.Size = new System.Drawing.Size(51, 16);
+            this.lblDealerScore.TabIndex = 1;
+            this.lblDealerScore.Text = "Dealer:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(588, 200);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(17, 16);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "\"\"";
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(836, 597);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(75, 23);
+            this.btnHit.TabIndex = 3;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // btnStand
+            // 
+            this.btnStand.Location = new System.Drawing.Point(836, 642);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(75, 23);
+            this.btnStand.TabIndex = 4;
+            this.btnStand.Text = "Stand";
+            this.btnStand.UseVisualStyleBackColor = true;
+            this.btnStand.Click += new System.EventHandler(this.btnStand_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(836, 685);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 5;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblDealerScore);
+            this.Controls.Add(this.lblPlayerScore);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPlayerScore;
+        private System.Windows.Forms.Label lblDealerScore;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStand;
+        private System.Windows.Forms.Button btnRestart;
     }
 }
 
--- Blackjack/Form1.cs
+++ Blackjack/Form1.cs
@@ -12,9 +12,81 @@ namespace Blackjack
 {
     public partial class Form1 : Form
     {
+       
+        private List<int> playerHand = new List<int>();
+        private List<int> dealerHand = new List<int>();
+        private Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            RestartGame();
+        }
+
+        private void RestartGame()
+        {
+            playerHand.Clear();
+            dealerHand.Clear();
+            lblResult.Text = "";
+
+            playerHand.Add(DrawCard());
+            playerHand.Add(DrawCard());
+
+            dealerHand.Add(DrawCard());
+            dealerHand.Add(DrawCard());
+
+            UpdateScores();
+        }
+
+        private int DrawCard()
+        {
+            return rand.Next(1, 11); // Card values from 1 to 10
+        }
+
+        private int GetScore(List<int> hand)
+        {
+            int total = 0;
+            foreach (int card in hand)
+                total += card;
+            return total;
+        }
+
+        private void UpdateScores()
+        {
+            lblPlayerScore.Text = "Player: " + GetScore(playerHand);
+            lblDealerScore.Text = "Dealer: " + GetScore(dealerHand);
+        }
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            playerHand.Add(DrawCard());
+            UpdateScores();
+
+            if (GetScore(playerHand) > 21)
+                lblResult.Text = "You busted!";
+        }
+
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            while (GetScore(dealerHand) < 17)
+                dealerHand.Add(DrawCard());
+
+            UpdateScores();
+
+            int playerScore = GetScore(playerHand);
+            int dealerScore = GetScore(dealerHand);
+
+            if (dealerScore > 21 || playerScore > dealerScore)
+                lblResult.Text = "You win!";
+            else if (dealerScore == playerScore)
+                lblResult.Text = "It's a tie!";
+            else
+                lblResult.Text = "Dealer wins!";
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            RestartGame();
         }
     }
-}
+   }
+
